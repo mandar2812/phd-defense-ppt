a819528eedc40e37ecdfee79e332e5bd353fd686
Mandar Chandorkar <mandar2812@gmail.com>
2019-11-10 17:32:30 +0100

    Initial commit

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+rm -rf ./*.aux ./*.bbl ./*.log ./*.blg ./*.out ./*.brf ./*.lol ./*.lot ./*.fls ./*.fdb_latexmk ./*.bcf ./*.lof
+rm -rf ./chapters/*.aux ./chapters/*.bbl ./chapters/*.log ./chapters/*.blg ./chapters/*.out ./chapters/*.brf ./chapters/*.lol ./chapters/*.lot ./chapters/*.fls ./chapters/*.fdb_latexmk ./chapters/*.bcf ./chapters/*.lof
